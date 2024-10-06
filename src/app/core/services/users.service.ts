fc24fa3a041b329be6933dbf8e133bb83b2f7379
omarxar71 <omarabdrabi123@gmail.com>
2024-10-06 13:33:10 +0300

    initial commit

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,13 @@ import { Injectable } from '@angular/core';
 export class UsersService {
 
   constructor() { }
+  private readonly _HttpClient=inject(HttpClient)
+  signUpAPI= (signUpForm:object):Observable <any>=>{
+    return this._HttpClient.post(`https://linked-posts.routemisr.com/users/signup` , signUpForm)
+  }
+  signInAPI= (signInForm:object):Observable <any>=>{
+    return this._HttpClient.post(`https://linked-posts.routemisr.com/users/signin` , signInForm)
+
+  }
+
 }
